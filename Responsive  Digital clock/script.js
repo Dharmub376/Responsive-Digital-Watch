91b6b91bd41f8c62061b6bf4afe93108138fd052
Dharmu Bhusal <110836127+Dharmub376@users.noreply.github.com>
2023-12-26 11:34:53 +0545

    Add files via upload

---
+++ Responsive  Digital clock/script.js
@@ -0,0 +1,15 @@
+function updateClock() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+
+  const digitalClock = document.getElementById('digital-clock');
+  digitalClock.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Initial call to display the clock immediately
+updateClock();
